cfd80623b8fe5c657034b854c0bdf6e50d1c8eb7
Jesse Quijano <jesse@quijano.net>
2018-12-29 19:35:03 -0800

    Updated tests to pass after removing route parameters and changing to querystring params

--- tests/Unit/JoybirdTest.php
+++ tests/Unit/JoybirdTest.php
@@ -15,31 +15,39 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class JoybirdTest extends TestCase
 {
-	private $headers = [
-		'CONTENT_TYPE' => 'application/json',
-		'HTTP_ACCEPT' => 'application/json',
-		'Cache-control' => 'no-cache',
-	];
-
+	/**
+	 * ensure when length is "wide open" we have the correct total number of records using POST route
+	 */
 	public function testTotalRecordsEquals184()
 	{
-		$this->withoutMiddleware();
-
-		$response = $this->call('get', '/1/500');
+		$response = $this->call('post', '/report?draw=1&start=0&length=500');
 
 		$obj = json_decode($response->getContent());
 
-		$this->assertEquals(184, count($obj));
+		$this->assertEquals(184, count($obj->data));
 	}
 
+	/**
+	 * ensure default gives us 25 records using GET route
+	 */
 	public function testTotalRecordsEquals25()
 	{
-		$this->withoutMiddleware();
-
-		$response = $this->call('get', '/');
+		$response = $this->call('get', '/report');
 
 		$obj = json_decode($response->getContent());
 
-		$this->assertEquals(25, count($obj));
+		$this->assertEquals(25, count($obj->data));
+	}
+
+	/**
+	 * ensure count endpoint provides correct value
+	 */
+	public function testRecordCountEquals184()
+	{
+		$response = $this->call('get', '/count');
+
+		$count = $response->getContent();
+
+		$this->assertEquals(184, $count);
 	}
 }
