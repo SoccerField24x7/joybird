12382e4e801eaa4574f8ea48c4f89e5ec5b129b7
Jesse Quijano <jesse@quijano.net>
2018-12-31 07:54:06 -0800

    convert page to utilize layout

--- resources/views/joybird.blade.php
+++ resources/views/joybird.blade.php
@@ -6,8 +6,46 @@
  * Time: 19:13
  */
 
-use App\Models\OrderCart;
+?>
+@extends('layout')
+@section('title')Joybird: DataTables Test @stop
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <table id="dtable" class="display">
+                <thead>
+                    <tr>
+                        <th>Bought Number</th>
+                        <th>Bought Item</th>
+                        <th>Sold Number</th>
+                        <th>Sold Item</th>
+                        <th>Default Purchase Price</th>
+                        <th>Container Unit pPrice</th>
+                        <th>Avg Sale Price (JAQ)</th>
+                        <th>Total Purchased</th>
+                        <th>Vendors</th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+        </div>
+    </div>
+@stop
+@section('javascript')
+    <script type="text/javascript">
+        $(document).ready(function() {
 
-$test = OrderCart::where('cart_id', '<', 3623)->get()->toArray();
-
-print_r($test);
+            $('#dtable').DataTable( {
+                "processing": true,
+                "serverSide": true,
+                ajax: {
+                    url: '/report',
+                    type: 'POST'
+                },
+                searching: false,
+                "order": [[ 7, "desc" ]]
+            });
+        });
+    </script>
+@stop
